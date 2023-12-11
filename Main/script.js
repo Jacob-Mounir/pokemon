284b90f5d3dc1cc6546ac89d05f2c6e818215b2b
Jacob Mounir <143597510+Jacob-Mounir@users.noreply.github.com>
2023-12-11 20:19:31 +0100

    funktion som fetchar pokmemon samtidigt i en gemensam lista

--- Main/script.js
+++ Main/script.js
@@ -1,24 +1,31 @@
 
 const fetchPokemon = () => {
 
-	for (let i = 1; i <= 150; i++) {
+
+	const promises = [] //Tom array
+	for (let i = 1; i <= 200; i++) {
 	const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-	fetch(url)
-		.then ((res) => res.json())
-		.then((data) =>{
-			console.log( '1' + data);
-			const pokemon = {};
-			pokemon['name'] = data.name
-			pokemon['id'] = data.id
-			pokemon['image'] = data.sprites
-			['front_default']
-			data.types.forEach( type => {
-				pokemon ['type'] = pokemon ['type'] + ", " + type.type.name
-			})
-			console.log(pokemon);
+	promises.push(fetch(url).then((res) => res.json()))
+	}
 
+	//KÖRS PARALLELT IST FÖR EN EFTER EN
+	Promise.all(promises).then(results => {
+		const pokemon = results.map( data => ({
+			name: data.name,
+			id: data.id,
+			image: data.sprites['front_default'],
+			type: data.types.map((type) => type.type.name).join(', ')
+		}))
+		displayPokemon(pokemon);
 	})
+}
 
-	}}
+const displayPokemon = (pokemon) => {
+	console.log(pokemon)
+
+
+
+}
+
 
 	fetchPokemon()

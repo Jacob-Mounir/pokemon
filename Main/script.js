34f7db817dcad540e8e42a57c3f05893149da43e
Jacob Mounir <143597510+Jacob-Mounir@users.noreply.github.com>
2023-12-11 19:55:11 +0100

    fethcar pokemons med namn, id, bild och typ

--- Main/script.js
+++ Main/script.js
@@ -7,9 +7,18 @@ const fetchPokemon = () => {
 		.then ((res) => res.json())
 		.then((data) =>{
 			console.log( '1' + data);
-		})
+			const pokemon = {};
+			pokemon['name'] = data.name
+			pokemon['id'] = data.id
+			pokemon['image'] = data.sprites
+			['front_default']
+			data.types.forEach( type => {
+				pokemon ['type'] = pokemon ['type'] + ", " + type.type.name
+			})
+			console.log(pokemon);
 
-		}
-	}
+	})
+
+	}}
 
 	fetchPokemon()

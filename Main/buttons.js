2ff19cd7a3de294b2b1a570f6c85fe904d2a9402
Jacob Mounir <143597510+Jacob-Mounir@users.noreply.github.com>
2023-12-12 21:31:18 +0100

    Lekt runt med headern och switchen mellan sidorna

--- Main/buttons.js
+++ Main/buttons.js
@@ -1,17 +1,19 @@
-const teamPage = document.getElementById("teamPage")
-const searchPage = document.getElementById("searchPage")
-const teamBtn = document.getElementById("team-btn")
-const searchBtn = document.getElementById("search-btn")
+const teamPage = document.getElementById("teamPage");
+const searchPage = document.getElementById("searchPage");
+const switchBtn = document.getElementById("switch-btn"); // Assuming you have a button with this ID
 
-teamBtn.addEventListener('click',()=> {
-
-	searchPage.classList.add('hide')
-	teamPage.classList.remove('hide')
-})
-
-searchBtn.addEventListener('click', () => {
-
-	searchPage.classList.remove('hide')
-	teamPage.classList.add('hide')
-})
-
+switchBtn.addEventListener('click', () => {
+	if (teamPage.classList.contains('hide')) {
+		// Show teamPage and hide searchPage
+		teamPage.classList.remove('hide');
+		searchPage.classList.add('hide');
+		// Update button text to indicate the current page is teamPage
+		switchBtn.textContent = 'Search Page';
+	} else {
+		// Show searchPage and hide teamPage
+		teamPage.classList.add('hide');
+		searchPage.classList.remove('hide');
+		// Update button text to indicate the current page is searchPage
+		switchBtn.textContent = 'Team Page';
+	}
+});

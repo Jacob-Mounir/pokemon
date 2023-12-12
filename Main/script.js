c2c627fb9e41694517e05b22cb4ff3bd683fd292
Jacob Mounir <143597510+Jacob-Mounir@users.noreply.github.com>
2023-12-12 11:52:58 +0100

    style in css, added button and font

--- Main/script.js
+++ Main/script.js
@@ -15,7 +15,7 @@ const fetchPokemons = () => {
 			name: data.name,
 			id: data.id,
 			image: data.sprites['front_default'],
-			type: data.types.map((type) => type.type.name).join(', ')
+			type: data.types.map((type) => type.type.name).join(", ")
 		}))
 		displayPokemon(pokemons);
 	})
@@ -24,12 +24,13 @@ const fetchPokemons = () => {
 const displayPokemon = (pokemons) => {
 	console.log(pokemons)
 	const pokemonsHTMLstring = pokemons.map (pokemon => `
-	<li>
-		<img src="${pokemon.image}"/>
-		<h2>${pokemon.id}. ${pokemon.name}</h2>
-		<p>Type: ${pokemon.type}</p>
-	</li>
-	` )
+	<li class="card">
+		<img class="card-image" src="${pokemon.image}"/>
+		<h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+		<p class="card-type">Type: ${pokemon.type}</p>
+		<button class="card-add-btn">Add To team</button>
+		</li>`)
+		.join("")
 	pokeList.innerHTML = pokemonsHTMLstring
 }
 

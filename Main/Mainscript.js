ada27115d0816861339bcce1e39dc7eceeeea587
Jacob Mounir <143597510+Jacob-Mounir@users.noreply.github.com>
2023-12-12 16:07:46 +0100

    lekte runt med en sökfunktion jag hittade

--- Main/Mainscript.js
+++ Main/Mainscript.js
@@ -17,27 +17,27 @@ const pokeList = document.getElementById("pokeList")
 
 
 
-	const promises = [] //Tom array
-	for (let i = 1; i <= 200; i++) {
+const promises = [] //Tom array
+for (let i = 1; i <= 200; i++) {
 	const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
 	promises.push(fetch(url).then((res) => res.json()))
-	}
+}
 
-	//KÖRS PARALLELT IST FÖR EN EFTER EN
-	Promise.all(promises).then(results => {
-		const pokemons = results.map( data => ({
-			name: data.name,
-			id: data.id,
-			image: data.sprites['front_default'],
-			type: data.types.map((type) => type.type.name).join(", ")
-		}))
-		displayPokemon(pokemons);
-	})
+//KÖRS PARALLELT IST FÖR EN EFTER EN
+Promise.all(promises).then(results => {
+	const pokemons = results.map(data => ({
+		name: data.name,
+		id: data.id,
+		image: data.sprites['front_default'],
+		type: data.types.map((type) => type.type.name).join(", ")
+	}))
+	displayPokemon(pokemons);
+})
 
 
 const displayPokemon = (pokemons) => {
 	console.log(pokemons)
-	const pokemonsHTMLstring = pokemons.map (pokemon => `
+	const pokemonsHTMLstring = pokemons.map(pokemon => `
 	<li class="card">
 		<img class="card-image" src="${pokemon.image}"/>
 		<h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
@@ -49,4 +49,4 @@ const displayPokemon = (pokemons) => {
 }
 
 
-	fetchPokemons()
+fetchPokemons()

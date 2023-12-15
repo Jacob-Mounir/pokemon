9dc60d702dfa8dc5f1cd0f74b5724d31b1fe1f08
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2023-12-15 14:24:59 +0100

    La till så att man kan lägga till och ta bort pokemons från reservlistan samt att det finns en counter i headern som räknar antalet pokemons

--- Main/buttons.js
+++ Main/buttons.js
@@ -1,4 +1,4 @@
-import { searchPokemon } from "./script.js";
+import {searchPokemon} from "./script.js";
 
 const teamPage = document.getElementById("teamPage");
 const searchPage = document.getElementById("searchPage");
@@ -6,14 +6,13 @@ const switchBtn = document.getElementById("switch-btn");
 
 const searchBar = document.getElementById("searchBar")
 
-const addBtn = document.getElementById("addBtn")
-
 switchBtn.addEventListener('click', () => {
 	if (teamPage.classList.contains('hide')) {
 
 		teamPage.classList.remove('hide');
 		searchPage.classList.add('hide');
 
+		switchBtn.classList.add('switch-btn')
 		switchBtn.innerText = 'Manage Team';
 
 	} else {
@@ -21,6 +20,7 @@ switchBtn.addEventListener('click', () => {
 		teamPage.classList.add('hide');
 		searchPage.classList.remove('hide');
 
+		switchBtn.classList.add('switch-btn')
 		switchBtn.innerText = 'Find Pokémon';
 	}
 });
@@ -35,13 +35,16 @@ searchBar.addEventListener('keyup', (e) => {
 })
 
 
-// ADD FUNCTION
-addBtn.addEventListener('click', () => {
-	// När man klickar så ska man denna Pokemon läggas in i reserv listan (En ny array antar jag, så då lär vi använda .map?)
 
-	// Listan ska först lägga till dom tre första pokemons man lägger till i de tomma div-elementen som heter ".slot" som ligger under section #searchPage. Resterande pokemons som man lägger till från SearchPage hamnar i Reserv. Via drag & drop ska du kunna lägga till reserverna i div-elementen slot. Om man lägger en pokemon från reserv listan så ska det som ligger i rutan hamna på första plats i reserv listan.
 
-	// När man har clickat på knappen så ska knappen ändra färg till grön och texten ska ändra till "Added"
 
-	//
-})
+
+// // ADD FUNCTION
+// addBtn.addEventListener('click', () => {
+// 	// När man klickar så ska man denna Pokemon läggas in i reserv listan (En ny array antar jag, så då lär vi använda .map?)
+
+// 	// Listan ska först lägga till dom tre första pokemons man lägger till i de tomma div-elementen som heter ".slot" som ligger under section #searchPage. Resterande pokemons som man lägger till från SearchPage hamnar i Reserv. Via drag & drop ska du kunna lägga till reserverna i div-elementen slot. Om man lägger en pokemon från reserv listan så ska det som ligger i rutan hamna på första plats i reserv listan.
+
+// 	// När man har clickat på knappen så ska knappen ändra färg till grön och texten ska ändra till "Added"
+
+// 	//
--- Main/script.js
+++ Main/script.js
@@ -1,13 +1,14 @@
+
 const pokeList = document.getElementById("pokeList")
 let Pokemon = new Object() // GLOBAL POKEMON LIST
-
-const addBtn = document.getElementById("addBtn")
-
-addBtn.addEventListener
-
-
-
 const promises = [] //Tom array
+
+// ADD TO MANGER
+const teamPokeList = document.getElementById("teamPokeList");
+const addBtn = document.getElementById("addBtn")
+const reserveList = [] //
+const reserveListElement = document.getElementById("reserveList");
+
 
 //Loop som hämtar data pokémons från API
 for (let i = 1; i <= 900; i++) {
@@ -17,7 +18,8 @@ for (let i = 1; i <= 900; i++) {
 
 
 // Hämtar pokemons från API
-Promise.all(promises).then(results => {
+Promise.all(promises)
+	.then(results => {
 	const pokemons = results.map(data => ({
 		name: data.name,
 		id: data.id,
@@ -27,9 +29,16 @@ Promise.all(promises).then(results => {
 	}))
 	Pokemon = pokemons
 	displayPokemon(pokemons);
+
+	const addButtons = document.querySelectorAll(".card-btn");
+	addButtons.forEach((button, index) => {
+		button.addEventListener("click", () => {
+		addToReserve(Pokemon[index]);
+		});
+	});
 })
 
-// ----- DISPLAY POKEMONS ------
+// ----- DISPLAY POKEMONS ON SEARCH PAGE ------
 const displayPokemon = (pokemons) => {
 	console.log(pokemons)
 	const pokemonsHTMLstring = pokemons.map(pokemon => `
@@ -42,9 +51,8 @@ const displayPokemon = (pokemons) => {
 		</li>`)
 		.join("")
 	pokeList.innerHTML = pokemonsHTMLstring
+
 }
-
-
 // -------SEARCH ----
 const searchPokemon = (searchString, pokemons = Pokemon) => {
 
@@ -56,29 +64,69 @@ const searchPokemon = (searchString, pokemons = Pokemon) => {
 	displayPokemon(filteredPokemon)
 }
 
-<<<<<<< HEAD
-export {searchPokemon}
-=======
-export { searchPokemon }
->>>>>>> origin
 
 
-// ----- MANAGE POKEMONS ----
+// ----- MANAGE POKEMONS ----q
 
-<<<<<<< HEAD
-const a = (pokemon) => {
-=======
-const addedPokemons = (pokemon) => {
->>>>>>> origin
-	console.log(pokemons)
-	const pokemonsHTMLstring = pokemons.map(pokemon => `
+function displayTeamPokemon(teamPokemons) {
+
+	const teamPokemonsHTMLstring = teamPokemons
+		.map((pokemon) => `
 	<li class="card">
-		<img class="card-image" src="${pokemon.image}"/>
-		<h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-		<p class="card-subtitle">Type: ${pokemon.type}</p>
-		<p class="card-subtitle">Abilities: ${pokemon.abilities}</p>
-		<button id="addBtn" class="card-btn">Add to Team</button>
-		</li>`)
-		.join("")
-	pokeList.innerHTML = pokemonsHTMLstring
+	<img class="card-image" src="${pokemon.image}"/>
+	<h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+	<p class="card-subtitle">Type: ${pokemon.type}</p>
+	<p class="card-subtitle">Abilities: ${pokemon.abilities}</p>
+	<button id="kickBtn" class="kick-btn">Kick from List</button>
+	</li>
+    `).join("");
+	teamPokeList.innerHTML = teamPokemonsHTMLstring;
+
+	const kickButtons = document.querySelectorAll(".kick-btn");
+	kickButtons.forEach((button, index) => {
+		button.addEventListener("click", () => {
+			const pokemonToRemove = teamPokemons[index];
+			removeFromReserve(pokemonToRemove);
+		});
+	});
 }
+
+
+const updateReserveList = (reserveList) => {
+	displayTeamPokemon(reserveList)
+}
+
+
+
+// --- COUNTER --- //
+
+
+function countPokemonsInReserve() {
+	return reserveList.length;
+}
+
+function updateCountInHTML (){
+	const countSpan = document.getElementById("count")
+	countSpan.textContent = countPokemonsInReserve()
+}
+
+const addToReserve = (pokemon) => {
+	reserveList.push(pokemon)
+	updateReserveList(reserveList)
+	updateCountInHTML()
+}
+
+function removeFromReserve(pokemon) {
+	const index = reserveList.findIndex((item) => item.id === pokemon.id);
+	if (index !== -1) {
+		reserveList.splice(index, 1);
+		updateReserveList(reserveList);
+		updateCountInHTML();
+	}
+}
+
+updateCountInHTML
+
+
+// ---- EXPORT ---- //
+export {searchPokemon}

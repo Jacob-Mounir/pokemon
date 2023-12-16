4646897154f5c7532ea13bf61c833e74efd6fdf4
Mr.Jacob <143597510+Jacob-Mounir@users.noreply.github.com>
2023-12-16 23:13:25 +0100

    added nickname button and the pokemons now adds to the fight card first

--- Main/script.js
+++ Main/script.js
@@ -6,8 +6,8 @@ const promises = [] //Tom array
 // ADD TO MANGER
 const teamPokeList = document.getElementById("teamPokeList");
 // const addBtn = document.getElementById("addBtn")
-const reserveList = [] //
-const reserveListElement = document.getElementById("reserveList");
+const teamList = [] //
+const reserveListElement = document.getElementById("teamList");
 const slotContainer = document.querySelector(".slot-container");
 const teamSlots = document.querySelectorAll(".slot");
 
@@ -27,32 +27,32 @@ for (let i = 1; i <= 900; i++) {
 // Hämtar pokemons från API
 Promise.all(promises)
 	.then(results => {
-	const pokemons = results.map(data => ({
-		name: data.name,
-		id: data.id,
-		image: data.sprites['front_default'],
-		type: data.types.map((type) => type.type.name).join(", "),
-		abilities: data.abilities.map((ability) => ability.ability.name).join(", ")
-	}))
-	Pokemon = pokemons
-	displayPokemon(pokemons);
+		const pokemons = results.map(data => ({
+			name: data.name,
+			id: data.id,
+			image: data.sprites['front_default'],
+			type: data.types.map((type) => type.type.name).join(", "),
+			abilities: data.abilities.map((ability) => ability.ability.name).join(", ")
+		}))
+		Pokemon = pokemons
+		displayPokemon(pokemons);
 
 
-	// ADD POKEMON + EFFECT
-	const addButtons = document.querySelectorAll(".card-btn");
-	addButtons.forEach((button, index) => {
-		button.addEventListener("click", () => {
-		addToReserve(Pokemon[index]);
+		// ADD POKEMON + EFFECT
+		const addButtons = document.querySelectorAll(".card-btn");
+		addButtons.forEach((button, index) => {
+			button.addEventListener("click", () => {
+				addToTeam(Pokemon[index]);
 
+			});
 		});
-	});
-})
+	})
 
 // ----- DISPLAY POKEMONS ON SEARCH PAGE ------
 const displayPokemon = (pokemons) => {
-		console.log(pokemons)
+	console.log(pokemons)
 
-const pokemonsHTMLstring = pokemons.map(pokemon => `
+	const pokemonsHTMLstring = pokemons.map(pokemon => `
 	<li class="card">
 		<img class="card-image" src="${pokemon.image}"/>
 		<h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
@@ -64,22 +64,19 @@ const pokemonsHTMLstring = pokemons.map(pokemon => `
 	pokeList.innerHTML = pokemonsHTMLstring
 
 }
-// -------SEARCH ---- //
+// ------- SEARCH & FILTER  ------
 const searchPokemon = (searchString, pokemons = Pokemon) => {
+	const filteredPokemon = pokemons.filter((pokemon) => {
 
-// -------FILTER ----
+		return (pokemon.name.includes(searchString) || pokemon.type.includes(searchString) || pokemon.id.toString().includes(searchString))
 
-const filteredPokemon = pokemons.filter((pokemon) => {
-
-	return (pokemon.name.includes(searchString) || pokemon.type.includes(searchString))
-
-})
+	})
 	displayPokemon(filteredPokemon)
 }
 
 
 
-// ----- MANAGE POKEMONS (REMOVE / ADD) ----q
+// ----- MANAGE POKEMONS (REMOVE / ADD) -----
 
 function displayTeamPokemon(teamPokemons) {
 
@@ -90,11 +87,19 @@ function displayTeamPokemon(teamPokemons) {
 	<h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
 	<p class="card-subtitle">Type: ${pokemon.type}</p>
 	<p class="card-subtitle">Abilities: ${pokemon.abilities}</p>
+	<form class="card-form">
+	<input type="text" class="card-input" placeholder="Nickname">
+	</form>
+	<div>
+	<button id="changeName" class="change-btn">Change name</button>
 	<button id="kickBtn" class="kick-btn">Kick from List</button>
+	</div>
 	</li>
     `).join("");
 	teamPokeList.innerHTML = teamPokemonsHTMLstring;
 
+
+	// KICK FROM TEAM BUTTON
 	const kickButtons = document.querySelectorAll(".kick-btn");
 	kickButtons.forEach((button, index) => {
 		button.addEventListener("click", () => {
@@ -108,12 +113,12 @@ function displayTeamPokemon(teamPokemons) {
 
 // --- COUNTER --- //
 
-//REMOVE FROM TEAM LIST 
+//REMOVE FROM TEAM LIST
 function removeFromTeam(pokemonToRemove) {
-	const index = reserveList.findIndex(pokemon => pokemon.id === pokemonToRemove.id);
+	const index = teamList.findIndex(pokemon => pokemon.id === pokemonToRemove.id);
 	if (index !== -1) {
-		reserveList.splice(index, 1);
-		updateReserveList(reserveList);
+		teamList.splice(index, 1);
+		updateReserveList(teamList);
 	}
 }
 
@@ -138,39 +143,48 @@ function updateTeamDisplay(team) {
 			}
 		}
 	});
-	updateReserveList(reserveList);
-}
-
-const updateReserveList = (reserveList) => {
-	displayTeamPokemon(reserveList)
+	updateReserveList(teamList);
 }
 
 
+// UPDATE THE RESERVE LIST
+const updateReserveList = (teamList) => {
+	displayTeamPokemon(teamList)
+}
+
+// ADD TO RESERVE
+const addToTeam = (pokemon) => {
+	if (team.length < maxTeamSize) {
+		team.push(pokemon);
+		updateTeamDisplay(team);
+	} else {
+		teamList.push(pokemon);
+		updateReserveList(teamList);
+	}
+	updateCountInHTML();
+};
+
+// COUNT POKEMONS
 function countPokemonsInReserve() {
-	return reserveList.length;
+	return teamList.length + team.length;
 }
 
-function updateCountInHTML (){
+
+// COUNTER UPDATE
+function updateCountInHTML() {
 	const countSpan = document.getElementById("count")
 	countSpan.textContent = countPokemonsInReserve()
 }
 
-const addToReserve = (pokemon) => {
-	reserveList.push(pokemon)
-	updateReserveList(reserveList)
-	updateCountInHTML()
-}
 
 function removeFromReserve(pokemon) {
-	const index = reserveList.findIndex((item) => item.id === pokemon.id);
-	if (index !== -1) {
-		reserveList.splice(index, 1);
-		updateReserveList(reserveList);
-		updateCountInHTML();
+	const index = teamList.indexOf(pokemon);
+	if (index > -1) {
+		teamList.splice(index, 1);
 	}
 }
 
 
 
 // ---- EXPORT ---- //
-export {searchPokemon}
+export { searchPokemon }
